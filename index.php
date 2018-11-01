7ab540ad6424b9dfd61b6a693bb4af9cfac0a141
Ivonne Garcia Santos <ivonnes118@gmail.com>
2018-11-01 06:17:29 +0000

    getting start

--- index.php
+++ index.php
@@ -1,9 +1,33 @@
+<?php
+include 'functions.php';
+    if(isset($_GET['query'])){
+        //Get access to API HERE
+    }
+
+?>
+
+
 <!DOCTYPE html>
 <html>
     <head>
-        <title> </title>
+        <title></title>
+        
+        <h1>WELCOME</h1>
+         <link href="css/style.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
-
+        <form method = "GET">
+              Product: <input type="text" name="productName" placeholder="Product keyword" /> <br />
+            <br>
+            Category: 
+            <select name="category">
+               <option value=" "> Select one </option>  
+            </select>
+            <br><br>
+            Price: From: <input type="text" name="priceFrom" size="5"/> 
+             To: <input type="text" name="priceTo" size="5"/>
+            <br><br>
+            <input type="submit" name="searchForm" value="Search" />
+        </form>
     </body>
 </html>
