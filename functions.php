931c13a67688a3bedcf8b1f6551f27dd2ac318c4
Sean Trehy <strehy@csumb.edu>
2018-11-02 23:26:35 +0000

    added displaySearchResults to functions

--- functions.php
+++ functions.php
@@ -15,4 +15,85 @@ function displayGenre(){
     }
 }
 
+function displaySeachResults(){
+        
+        global $dbConn;
+        
+        if(isset($_GET['searchForm'])){
+            
+            echo "<h3>Moives Found: </h3>";
+            
+            $namedParameters = array();
+            
+            $sql = "SELECT * FROM  WHERE 1"; //ADD DATABASE NAME
+            
+            //Check input fields
+            if(!empty($_GET['MovieName'])){
+                $sql .= " AND productName OR productDescription LIKE :MovieName"; //CHANGE SQL SEARCH VALUES
+                $namedParameters[':MovieName'] = "%" .$_GET['MovieName']. "%";
+            }
+            
+            if(!empty($_GET['genre'])){
+                $sql .= " AND catId = :genre"; //CHANGE SQL SEARCH VALUES
+                $namedParameters[':genre'] = $_GET['genre'];
+            }
+            
+            if(!empty($_GET['priceFrom']) && !empty($_GET['priceTo'])){
+                $sql .= " AND price >= :priceFrom"; //CHANGE SQL SEARCH VALUES
+                $sql .= " AND price <= :priceTo"; //CHANGE SQL SEARCH VALUES
+                $namedParameters[':priceFrom'] = $_GET['priceFrom'];
+                $namedParameters[':priceTo'] = $_GET['priceTo'];
+            }
+            
+            if(isset($_GET['orderBy'])){
+                if($_GET['orderBy'] == 'price'){
+                    $sql .= " ORDER BY price"; //CHANGE SQL SEARCH VALUES
+                }
+                else{
+                    $sql .= " ORDER BY productName"; //CHANGE SQL SEARCH VALUES
+                }
+            }
+            
+            $stmt = $db->prepare($sql);
+            $stmt->execute($namedParameters);
+            $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            
+            //Output results
+            echo "<table class='table'>";
+            foreach($records as $record){
+            
+            //CHECK DB VALUES
+            $movieName = $record['MovieName'];
+            $genre = $record['genre'];
+            $itemImage = $record['thumbnailImage'];
+            
+            
+            //CHANGE VALUES TO CURRENT DB
+            echo "<tr>";
+            echo "<td><img src='$itemImage'></td>";
+            echo "<td><h4>$itemName</h4></td>";
+            echo "<td><h4>$itemPrice</h4></td>";
+            
+            //UPDATE VARIABLES TO 
+            echo "<forum method='post'>";
+            echo "<input type='hidden' name='itemName' value='$itemName'>";
+            echo "<td><button class='btn btn-warning'>Add</button></td>";
+            echo "</forum>";
+            
+            echo "<tr>";
+            
+            echo "<forum method='post'>";
+            echo "<input type='hidden' name='itemName' value'$itemName'>";
+            echo "<input type='hidden' name='itemId' value'$itemId'>";
+            echo "<input type='hidden' name='itemImage' value'$itemImage'>";
+            echo "<input type='hidden' name='itemPrice' value'$itemPrice'>";
+            
+            echo "<td><button class='btn btn-warning'>ADD</button></td>";
+            echo "</forum>";
+            
+        }
+        echo "</table>";
+        }
+    }
+
 ?>
